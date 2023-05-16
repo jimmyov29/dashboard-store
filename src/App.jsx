6baee3c500e5b32c5970996207653a7862a36cf4
jimmyov29 <jimmy.ov2998@gmail.com>
2023-05-16 10:40:55 -0600

    empezando el main

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import React from "react";
 import { Sidebar } from "./components/shared/Sidebar";
-import { RiAddLine, RiLightbulbLine, RiMenu2Fill, RiPieChart2Line, RiUser2Fill } from "react-icons/ri";
+import { RiAddLine, RiCloseLine, RiLightbulbLine, RiMenu2Fill, RiPieChart2Line, RiUser2Fill } from "react-icons/ri";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+  const toggleMenu = ()=>{
+    setShowMenu(!showMenu);
+  }
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar />
-      {/* Menu Movil */}
-      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-4 px-8 flex items-center justify-between
+      <Sidebar  showMenu = {showMenu}/>
+      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between
       rounded-tl-xl rounded-tr-xl" >
-        <button>
+        <button className="p-2">
           <RiUser2Fill />
         </button>
-        <button>
+        <button className="p-2">
           <RiAddLine />
         </button>
-        <button>
+        <button className="p-2">
           <RiPieChart2Line />
         </button>
-        <button className="text-white">
-          <RiMenu2Fill />
+        <button onClick={toggleMenu} className="text-white p-2">
+        {showMenu ? <RiCloseLine /> : <RiMenu2Fill /> }
         </button>
       </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
+      <div className="lg:col-span-6 bg-red-400">Hola 1</div>
+      <div className="lg:col-span-2 bg-red-200"> productos</div>
+
+      </main>
     </div>
   );
 }
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -9,9 +9,11 @@ import {
   RiSettings2Line,
 } from "react-icons/ri";
 
-export const Sidebar = () => {
+export const Sidebar = (props) => {
+    const {showMenu} = props;
   return (
-    <div className="bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full">
+    <div className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50  transition-all
+    ${showMenu ? "left-0" : "-left-full"}`}>
       <div>
         <ul className="pl-4">
           <li>
